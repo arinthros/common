05b5d1aa4229ad20830ea9495c76deabac8fdc49
arinthros <arinthros@gmail.com>
2020-12-31 18:49:58 -0600

    Add flag to use npm instead of yarn

--- src/scripts/reinstall.js
+++ src/scripts/reinstall.js
@@ -7,12 +7,27 @@
  * @example
  * yarn reinstall
  */
+const fs = require('fs')
 const { execSync } = require('child_process')
 
 // eslint-disable-next-line no-console
 console.log('[0/4] Cleaning up untracked files...')
 
 const rmCommand = process.platform === 'win32' ? 'rmdir /Q /S' : 'rm -rf'
-execSync(`${rmCommand} node_modules && ${rmCommand} publish && yarn install`, {
+
+const folders = ['node_modules', 'publish', 'watch']
+folders.forEach((folder) => {
+  if (fs.existsSync(folder)) {
+    execSync(`${rmCommand} ${folder}`)
+  }
+})
+
+let packageManager = 'yarn'
+if (process.argv.slice(2).includes('--npm')) {
+  packageManager = 'npm'
+}
+// Run the install command
+// TODO: detect which package manager fired the command
+execSync(`${packageManager} install`, {
   stdio: 'inherit',
 })
