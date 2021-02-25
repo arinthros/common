3b45defe0dedce521a50f0e409ea0c7daf90c3f9
arinthros <arinthros@gmail.com>
2021-02-25 08:25:25 -0600

    Add build script

---
+++ src/scripts/build.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+/**
+ * Creates publish directory with relevant files.
+ *
+ * @module build
+ *
+ * @example
+ * yarn build
+ */
+const fs = require('fs-extra')
+const path = require('path')
+
+// eslint-disable-next-line no-console
+console.log('Building the package...')
+
+const publishPath = path.join(process.cwd(), 'publish')
+
+fs.removeSync(publishPath)
+fs.mkdirSync(publishPath)
+
+const filesToCopy = ['package.json', 'README.md']
+const foldersToCopy = ['src']
+
+const promises = [
+  ...filesToCopy.map((file) => {
+    return fs.copy(path.join(process.cwd(), file), path.join(publishPath, file))
+  }),
+  ...foldersToCopy.map((folder) => {
+    return fs.copy(path.join(process.cwd(), folder), publishPath)
+  }),
+]
+
+Promise.all(promises)
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Package built.')
+    process.exit(0)
+  })
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error(error.message)
+    process.exit(1)
+  })
